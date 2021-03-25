f1372f3badef1546c50d7b821340bf516288ad1c
unknown <elsayedkamal581999@gmail.com>
2021-03-25 23:13:24 +0200

    “wip”

--- app/Http/Requests/Iot/V1/Car/UpdateCurrentLocation.php
+++ app/Http/Requests/Iot/V1/Car/UpdateCurrentLocation.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Iot\V1\Car;
 use App\Models\Drive\Car;
 use App\Rules\LocationCoordinates\LatitudeRule;
 use App\Rules\LocationCoordinates\LongitudeRule;
-use BultonFr\NMEA\Parser;
+use App\Services\Location\Parser;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateCurrentLocation extends FormRequest
@@ -36,8 +36,7 @@ class UpdateCurrentLocation extends FormRequest
     protected function prepareForValidation(): void
     {
         $location = $this->location;
-        $data = (object)(new Parser())->readLine($location);
-        //dd($data);
+        $data = (object)(new Parser())->readLine($location)->toArray();
         $this->merge([
             'lat' => (float) $data->latitude,
             'lng' => (float) $data->longitude,
