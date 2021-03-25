403612cb3b6dde2e1eb12e146cf8c9215af4b655
unknown <elsayedkamal581999@gmail.com>
2021-03-25 23:42:37 +0200

    “wip”

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //$schedule->command("queue:restart")->everyMinute();
-        $schedule->command("queue:work --tries=3")->everyMinute()->withoutOverlapping();
-        $schedule->command("queue:work --tries=3")->everyMinute()->withoutOverlapping();
+        $schedule->command("queue:work --tries=3")->everyMinute();
+        $schedule->command("queue:work --tries=3")->everyMinute();
         $schedule->command('websockets:clean')->daily();
     }
 
